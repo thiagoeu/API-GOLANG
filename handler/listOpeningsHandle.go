e0bf1c5c32096e608d2cf361fdcd94967d13cbe3
thiagoeu <araujo.thiago1051@gmail.com>
2023-08-19 18:39:50 -0300

    feat:: list openings

--- handler/listOpeningsHandle.go
+++ handler/listOpeningsHandle.go
@@ -4,10 +4,16 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/thiagoeu/GOstd/schemas"
 )
 
 func ListOpeningsHandle(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "LIST opening",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+
+	sendSucess(ctx, "list-openings", openings)
 }
